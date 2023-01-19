172e6207826815eef990fc89c91cbc404e7db773
unknown <mujaheedsami13@gmail.com>
2023-01-19 23:58:46 +0530

    Completed Day-3

--- Backend/routes/products.route.js
+++ Backend/routes/products.route.js
@@ -1,4 +1,4 @@
-const {Router} = require("express");
+const {Router, query} = require("express");
 const { ProductModel } = require("../models/product.model.js");
 const productRouter = Router();
 
@@ -7,6 +7,39 @@ productRouter.post("/add", async(req, res)=>{
         const product = ProductModel(req.body);
         await product.save();
         res.send("Product added");
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+productRouter.get("/", async(req, res)=>{
+    const category = req.query.category;
+    const order = req.query.sort;
+    try {
+        if(category){
+            const data = await ProductModel.find({category});
+            res.json(data);
+        } else if(order && category){
+            if(order=="asc"){
+                const data = await ProductModel.find({category}).sort({price: 1});
+                res.json(data);
+            }else if(order=="dsc"){
+                const data = await ProductModel.find({category}).sort({price: -1});
+                res.json(data);
+            }
+        } else if(order){
+            if(order=="asc"){
+                const data = await ProductModel.find().sort({price: 1});
+                res.json(data);
+            }else if(order=="dsc"){
+                const data = await ProductModel.find().sort({price: -1});
+                res.json(data);
+            }
+        }else {
+            const data = await ProductModel.find();
+            res.json(data);
+        }
     } catch (error) {
         console.log(error);
         res.send(error);
